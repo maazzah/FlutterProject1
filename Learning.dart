a71b971f7400e2e261e169c196110d714087fa3e
MAAZ <maaz@MAAZs-Laptop.local>
2024-12-09 15:15:21 +0530

    Initial commit

---
+++ Learning.dart
@@ -0,0 +1,42 @@
+//FUNCTIONS
+
+/*void main(){
+showOutput(cube(4));
+print(cube.runtimeType);
+}
+    dynamic cube(var num){
+        return num * num * num;
+    }
+void showOutput(var msg){
+    print(msg);
+}   
+
+//SHORTHAND TO WRITE A FUNCTION
+
+void main(){
+showOutput(cube(4));
+print(cube.runtimeType);
+}
+    dynamic cube(var num) => num * num * num;
+    
+void showOutput(var msg){
+    print(msg);
+}*/
+
+//ANONYMOUS FUNCTION or NAMELESS FUNCTION (In Python- Lambda Function)
+ void main(){
+    var fruits = {'Apple','Banana','Kiwi','Orange'};
+
+   //NORMALLY ESE LIKHTE FUNCTION LIST KO FOR EACH USE KRKE CALL KRNE KE LIYE
+   /* fruits.forEach(printF);
+ }
+    void printF(item){
+        print(item);
+    }
+//Anonymous function me seedha function dete bina name ke forEach loop me, like:- 
+    fruits.forEach((item){
+        print(item);
+ });
+ }
+ */
+ }
